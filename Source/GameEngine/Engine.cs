c2cffdf7cb76d999a69479ea798469c3f977fbd6
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-03-30 13:38:40 +0200

    Initial commit, created projects to fill with classes.

---
+++ Source/GameEngine/Engine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    /* Classes in this namespace are meant to modify the game's state based on inputs, doing things such as rolling dice and moving figures around the board.
+     */
+    class Engine
+    {
+    }
+}
---
+++ Source/Renderer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Renderer
+{
+    /* Classes in this namespace are used to render the game on screen based on the current game state.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

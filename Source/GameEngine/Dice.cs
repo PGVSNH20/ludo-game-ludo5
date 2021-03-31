6802e422dd9af7ad26a962d89adccb2055c1ba0f
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-03-31 10:25:29 +0200

    Dice class made.

---
+++ Source/GameEngine/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    static class Dice
+    {
+        public static int Roll()
+        {
+            return new Random().Next(1, 7);
+        }
+    }
+}

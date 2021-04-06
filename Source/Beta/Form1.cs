1dcf9e65ea6c461cfe8bb55b20f32413b924d815
Sanna Edström <70255968+Cimeiesh@users.noreply.github.com>
2021-04-06 16:05:49 +0200

    change number on board

--- Source/Beta/Form1.cs
+++ Source/Beta/Form1.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using GameEngine;
+using GameEngine.Classes;
 
 namespace Beta
 {
     public partial class Form1 : Form
     {
+        public int players { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
+            
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Engine newEngine = new Engine(new GameSettings(4, 44));
+            newEngine.StartGame();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -43,6 +44,11 @@ namespace Beta
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }

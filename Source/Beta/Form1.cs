59e20b0d7a9ee31c084615d86d91363365c03810
Sanna Edström <70255968+Cimeiesh@users.noreply.github.com>
2021-04-08 11:57:06 +0200

    Update form1

--- Source/Beta/Form1.cs
+++ Source/Beta/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 using GameEngine;
 using GameEngine.Classes;
@@ -9,13 +10,16 @@ namespace Beta
 {
     public partial class Form1 : Form
     {
+        public readonly object count;
 
         public Form1()
         {
+            AllocConsole();
             InitializeComponent();
-
         }
 
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
 
         //Kasta tärningen
         public void button1_Click(object sender, EventArgs e)
@@ -35,7 +39,6 @@ namespace Beta
 
             newEngine.StartGame();
 
-            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -67,6 +70,8 @@ namespace Beta
             {
                 listBox1.Items.Add(player);
             }
+
+            newEngine.StartGame();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)

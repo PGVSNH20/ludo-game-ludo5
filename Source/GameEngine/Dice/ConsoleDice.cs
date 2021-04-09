1c448a0ab92f79b5958c033084bcbf6105ff7af7
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-04-09 11:07:44 +0200

    ConsoleDice för spelare med CLI.

---
+++ Source/GameEngine/Dice/ConsoleDice.cs
@@ -0,0 +1,20 @@
+﻿using GameEngine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Dice
+{
+    class ConsoleDice : IDice
+    {
+        public int Roll()
+        {
+            Console.WriteLine("Press Enter to roll the dice");
+            Console.ReadLine();
+            var roll = new Random();
+            return roll.Next(1, 7);
+        }
+    }
+}

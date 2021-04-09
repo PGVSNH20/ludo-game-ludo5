aa99f4c9987f9a85affae81190576a7f546e0e3c
Rvonkrusenstjerna <70213477+Rvonkrusenstjerna@users.noreply.github.com>
2021-04-09 14:52:11 +0200

    Added a Knockout clause.

--- Source/GameEngine/Selectors/AISelector.cs
+++ Source/GameEngine/Selectors/AISelector.cs
@@ -14,7 +14,40 @@ namespace GameEngine.Selectors
         public Turn Selector(Turn currentTurn, List<int> selectionList)
         {
             currentTurn.PieceID = selectionList[0];
+
+            
+
             return currentTurn;
         }
+
+
+        public bool KnockOut(int pieceId, Turn currentturn, Board board)
+        {
+            int LandingSpot = board.Pieces[pieceId].PiecePosition + (int)currentturn.Roll;
+
+            if (!board.MainBoard[LandingSpot].Safe)
+            {
+                
+                foreach (Piece item in board.Pieces)
+                {
+                    if (LandingSpot == item.PiecePosition && item.PlayerID != board.Pieces[pieceId].PlayerID)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+        return false; 
+
+        }
+
+
+
+
+        ///1 Move out of nest.
+        ///2 Knock away oponents piece.
+        ///3 Move to Goal.
+        ///4 Move random piece. 
     }
 }

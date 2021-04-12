2f0e9fb1869694b7c7ffc48454635896fabbb541
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-04-12 11:23:00 +0200

    Tog bort oanvända usings i Renderer-projektets klasser.

--- Source/Renderer/Program.cs
+++ Source/Renderer/Program.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using GameEngine;
-using GameEngine.Models;
-using GameEngine.Selectors;
-using GameEngine.Dice;
-using System.Diagnostics;
+﻿using System.Threading;
+
 namespace Renderer
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            var renderer = new ConsoleRenderer();
-            renderer.Setup().Start();
-            //Thread.Sleep(100);
-            //var renderer2 = new ConsoleRenderer();
-            //renderer2.Setup().Start();
-            //OneHundred.CountThis();
+            //var renderer = new ConsoleRenderer();
+            //renderer.Setup().Start();
+            Thread.Sleep(100);
+            var renderer2 = new ConsoleRenderer();
+            renderer2.Setup().Start();
+            OneHundred.CountThis();
         }
     }
 }
